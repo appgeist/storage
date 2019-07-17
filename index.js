f9e0eefa8793f5cc35cca4eb4b31179708045058
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-07-18 00:56:35 +0300

    Refactoring and improving...

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const express = require('express');
-const morgan = require('morgan');
-const compression = require('compression');
-const cors = require('cors');
-const helmet = require('helmet');
 
 const upload = require('./lib/upload');
 const serve = require('./lib/serve');
@@ -13,39 +9,40 @@ const PRODUCTION = process.env.NODE_ENV === 'production';
 /**
  * Create a storage server Express-based instance
  * @param {Object} [options]
- * @param {string} [options.logPattern] Log pattern provided to morgan,
- *    defaults to `:method :url :status - :response-time ms` in production and `dev` in development
- * @param {string} [options.rootDir] Folder to store files (relative to `cwd()`),
- *    defaults to `./files`
+ * @param {string} [options.storageDir] Folder to store files (relative to `cwd()`),
+ *    defaults to `./storage`
  * @param {string} [options.tmpDir] Temp folder used for uploading files,
  *    defaults to `require('os').tmpdir` in production and `./temp` relative to `cwd()` in development
- * @param {number} [options.maxFileSize] Maximum upload file size in bytes, defaults to `1024 * 1024 * 20` (20MB)
- * @param {number} [options.maxPicturePixels] Maximum upload file size in pixels,
+ * @param {number} [options.maxUploadSize] Maximum upload file size in bytes, defaults to `1024 * 1024 * 20` (20MB)
+ * @param {number} [options.maxPicturePixels] Maximum stored file size in pixels,
  *    defaults to `1920 * 1080` (FullHD ~ 2 megapixels)
- * @param {number} [options.maxUrlCacheEntries] Maximum number of entries to keep in the file info cache,
+ * @param {number} [options.maxUrlCacheItems] Maximum number of entries to keep in the file info cache,
  *    defaults to `10000000` (one million items)
  * @param {number|string} [options.maxAge] maxAge of served files,
  *    a number representing milliseconds or a string in [ms format](https://www.npmjs.com/package/ms);
  *    defaults to `365 days` in production and `0` in development;
- * @param {tokenValidationHandler} [options.tokenValidationHandler] Function used to validate an upload request
- *    based on its Authentication header value
+ * @param {uploadTokenValidationHandler} [options.tokenValidationHandler] Function used to validate an upload POST
+ *    request based on its Authentication header value
+ * @param {serveTokenValidationHandler} [options.tokenValidationHandler] Function used to validate a GET
+ *    request based on its Authentication header value
  * @returns {import('express')} Express instance
  */
 module.exports = (options) => {
   const {
-    logPattern,
-    rootDir,
+    storageDir,
     tmpDir,
-    maxFileSize,
+    maxUploadSize,
+    pictureQuality,
     maxPicturePixels,
-    maxUrlCacheEntries,
+    maxUrlCacheItems,
     maxAge,
-    tokenValidationHandler
+    uploadTokenValidationHandler,
+    serveTokenValidationHandler
   } = {
-    logPattern: PRODUCTION ? ':method :url :status - :response-time ms' : 'dev',
-    rootDir: './files',
+    storageDir: './storage',
     tmpDir: PRODUCTION ? require('os').tmpdir : './temp',
-    maxFileSize: 1024 * 1024 * 20, // 20 megabytes
+    maxUploadSize: 1024 * 1024 * 20, // 20 megabytes
+    pictureQuality: 85, // 85%
     maxPicturePixels: 1920 * 1080, // FullHD ~ 2 megapixels
     maxUrlCacheEntries: 10000000, // one million items
     maxAge: PRODUCTION ? '365 days' : 0, // one year for production, none for development
@@ -54,13 +51,25 @@ module.exports = (options) => {
 
   const app = express();
 
-  if (logPattern) app.use(morgan(logPattern));
-  app.use(compression());
-  app.use(cors());
-  app.use(helmet());
   app.use(express.json());
-  app.use(upload({ rootDir, tmpDir, maxFileSize, maxPicturePixels, tokenValidationHandler }));
-  app.use(serve({ rootDir, maxUrlCacheEntries, maxAge }));
+  app.use(
+    upload({
+      storageDir,
+      tmpDir,
+      maxUploadSize,
+      pictureQuality,
+      maxPicturePixels,
+      uploadTokenValidationHandler
+    })
+  );
+  app.use(
+    serve({
+      storageDir,
+      maxUrlCacheItems,
+      maxAge,
+      serveTokenValidationHandler
+    })
+  );
   app.use(errorHandler);
 
   return app;
